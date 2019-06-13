4745cfd72197d75f559ff594e504b140e0ed5259
dandisy <dandi.webdev@gmail.com>
2019-06-13 09:01:41 +0700

    support closure

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 | Example API query :
 | https://your-domain-name/JSON/Post?leftJoin=comments,posts.id,comments.post_id&whereIn=category_id,[2,4,5]&select=*&get=
 | https://your-domain-name/JSON/Post?join[]=authors,posts.id,authors.author_id&join[]=comments,posts.id,comments.post_id&whereIn=category_id,[2,4,5]&select=posts.*,authors.name as author_name,comments.title as comment_title&get=
-| https://your-domain-name/JSON/Post?&with=author,comment&select=*&get=
+| https://your-domain-name/JSON/Post?&with=author,comment&get=*
+| https://your-domain-name/JSON/Post?&with=author(where=name,like,%dandisy%),comment&get=*
+| multi first nested closure deep
+| https://your-domain-name/JSON/Post?&with=author(where=name,like,%dandisy%)(where=nick,like,%dandisy%),comment&get=*
+| second nested closure deep
+| https://your-domain-name/JSON/Post?&with=author(with=city(where=name,like,%jakarta%)),comment&get=*
+| https://your-domain-name/JSON/Post?&with[]=author(where=name,like,%dandisy%)&with[]=comment(where=title,like,%test%)&get=*
 | https://your-domain-name/JSON/Post?paginate=10&page=1
 |
 */
@@ -65,16 +71,19 @@ Route::get('JSON/{model}/{id?}', function(Request $request, $model, $id = NULL) 
                 array_push($vals, $val);
             }
 
-            foreach($vals as $item) {                    
-                if(preg_match('/\[(.*?)\]/', $item, $match)) { // due to whereIn, the $val using [...] syntax
-                    $item = str_replace(','.$match[0], '', $item);
-                    $item = explode(',', $item);
-                    array_push($item, explode(',',$match[1]));
-                } else {
-                    $item = explode(',', $item);
-                }
+            foreach($vals as $item) {
+                // if(preg_match('/\[(.*?)\]/', $item, $match)) { // due to whereIn, the $val using [...] syntax
+                //     $item = str_replace(','.$match[0], '', $item);
+                //     $item = explode(',', trim($item));
+                //     array_push($item, explode(',', trim($match[1])));
+                // } else {
+                //     $item = explode(',', item($item));
+                // }
 
-                $data = call_user_func_array(array($data,$key), $item);
+                // $data = call_user_func_array(array($data,$key), $item);
+
+                $data = getData($data, $key, $item);//['data'];
+
             }
 
             if($key === 'paginate') {
@@ -82,7 +91,7 @@ Route::get('JSON/{model}/{id?}', function(Request $request, $model, $id = NULL) 
             }
         }
     }
-    
+
     return $data;
 })->middleware('auth:api');
 
@@ -130,9 +139,203 @@ Route::delete('JSON/{model}/{id}', function($model, $id) {
         return $data->delete();
     }
 })->middleware('auth:api');
-// End global API for direct model class under Models directory
 
-// using client credentials
+/*
+|--------------------------------------------------------------------------
+| EloREST - Using client credentials
+|--------------------------------------------------------------------------
+|
+*/
 Route::group(['middleware' => 'client_credentials'], function () {
-    // your public route resource here!
+    // EloREST script here!
 });
+
+
+/*
+|--------------------------------------------------------------------------
+| EloREST - Halpers
+|--------------------------------------------------------------------------
+|
+| getData
+|
+*/
+function getData($data, $key, $param) {
+    // if(preg_match_all('/\((.*?)\)/', $request->test, $match)) { // multi occurence
+    //     return $match;
+    // }
+
+    // if(preg_match('/(.*?)\((.*?)\)/', $param, $closureMatch)) { // handling closure, for now only support once nested closure
+    //     $param = str_replace('('.$closureMatch[2].')', '', $param);
+    //     $param = explode(',', $param);
+
+    //     foreach($param as $par) {
+    //         if($par == $closureMatch[1]) {
+    //             $data = $data->$key([$closureMatch[1] => function($closureQuery) use ($closureMatch) {
+    //                 $closureParams = explode('=', trim($closureMatch[2]));
+    //                 $closureParam = getData($closureQuery, $closureParams[0], $closureParams[1])['param'];
+
+    //                 call_user_func_array(array($closureQuery,$closureParams[0]), $closureParam);
+    //             }]);
+    //         } else {
+    //             $data = $data->$key($par);
+    //         }
+    //     }
+    if(preg_match_all("/\((([^()]*|(?R))*)\)/", $param, $closureMatch)) { // handling closure, for now only support once nested closure
+        // $closureMatch[1] = [
+        //     "contactPerson(with=phone(where=city_code,021))(where=first_name,like,%test%)",
+        //     "organization(where=name,like,%test%)",
+        //     "product"
+        // ]
+        $arrayParam = recursiveParam($param);
+        if(count($arrayParam) > 0) {
+            $data = recursiveQuery($data, $key, $param, $closureMatch, $arrayParam);//['data'];
+        }
+    } else {
+        if(preg_match('/\[(.*?)\]/', $param, $arrParamMatch)) { // handling whereIn, due to whereIn params using whereIn('field', ['val_1', 'val_2', 'val_n']) syntax
+            $param = str_replace(','.$arrParamMatch[0], '', $param);
+            $param = explode(',', trim($param));
+            array_push($param, explode(',', trim($arrParamMatch[1])));
+        } else {
+            $param = explode(',', trim($param));
+        }
+
+        $data = call_user_func_array(array($data,$key), $param);
+    }
+
+    // return [
+    //     'param' => $param,
+    //     'data' => $data
+    // ];
+    return $data;
+}
+
+/*
+|--------------------------------------------------------------------------
+| EloREST - Halpers
+|--------------------------------------------------------------------------
+|
+| recursiveQuery
+|
+*/
+function recursiveQuery($data, $key, $param, $matches, $arrayParam) {
+    // $arr = [
+    //     "with=phone(where=city_code,021),where=city_code,021",
+    //     "where=first_name,like,%test%"
+    // ]
+    foreach($matches[1] as $item) {
+        $param = str_replace('('.$item.')', '|', $param); // signing using '|' for closure
+    }
+
+    $params = explode(',', $param);
+    foreach($params as $i => $param) {
+        if (strpos($param, '|')) {
+            $param = rtrim($param, '|');
+            $items = explode('|', $arrayParam[$i]);
+
+            if(count($items) > 1) {
+                $data = $data->$key([$param => function($query) use ($items) {
+                    recursiveClosure($items);
+                    // this, only support second nested closure deep
+                    // foreach($items as $idx => $val) {
+                    //     if($idx < count($items)-1) {
+                    //         $closureParam = $items[$idx+1];
+                    //         $closure = str_replace('('.$closureParam.')', '', $val);
+
+                    //         $closureData = explode('=', trim($closure));
+
+                    //         $query = $query->$closureData[0]([$closureData[1] => function($query) use ($closureParam) {
+                    //             $closureParams = explode('=', trim($closureParam));
+
+                    //             call_user_func_array(array($query,$closureParams[0]), explode(',', trim($closureParams[1])));
+                    //         }]);
+                    //     }
+                    // }
+                }]);
+            } else {
+                $item = $matches[1][$i];
+
+                $data = $data->$key([$param => function($query) use ($item) {
+                    $params = explode('=', trim($item));
+
+                    call_user_func_array(array($query,$params[0]), explode(',', trim($params[1])));
+                }]);
+            }
+        } else {
+            $data = call_user_func_array(array($data,$key), [$param]);
+        }
+    }
+
+    // return [
+    //     'param' => $param,
+    //     'data' => $data
+    // ];
+    return $data;
+}
+
+/*
+|--------------------------------------------------------------------------
+| EloREST - Halpers
+|--------------------------------------------------------------------------
+|
+| recursiveClosure
+|
+*/
+function recursiveClosure($items) {
+    foreach($items as $idx => $val) {
+        if($idx < count($items)-2) {
+            $closureParam = $items[$idx+1];
+            $closure = str_replace('('.$closureParam.')', '', $val);
+            $closureData = explode('=', trim($closure));
+
+            $query = $query->$closureData[0]([$closureData[1] => function($query) use ($items) {
+                recursiveClosure(array_shift($items));
+            }]);
+        } else {
+            if($idx < count($items)-1) {
+                $closureParam = $items[$idx+1];
+                $closure = str_replace('('.$closureParam.')', '', $val);
+                $closureData = explode('=', trim($closure));
+
+                $query = $query->$closureData[0]([$closureData[1] => function($query) use ($closureParam) {
+                    $closureParams = explode('=', trim($closureParam));
+
+                    call_user_func_array(array($query,$closureParams[0]), explode(',', trim($closureParams[1])));
+                }]);
+            }
+        }
+    }
+}
+
+/*
+|--------------------------------------------------------------------------
+| EloREST - Halpers
+|--------------------------------------------------------------------------
+|
+| recursiveParam
+|
+*/
+function recursiveParam($param) {
+    $layer = 0;
+    $arrayParam = [];
+
+    preg_match_all("/\((([^()]*|(?R))*)\)/", $param, $matches);
+    if (count($matches) > 1) {
+        for ($i = 0; $i < count($matches[1]); $i++) {
+            if (is_string($matches[1][$i])) {
+                if (strlen($matches[1][$i]) > 0) {
+                    array_push($arrayParam, $matches[1][$i]);
+
+                    $res = recursiveParam($matches[1][$i], $layer + 1);
+
+                    if(count($res) > 0) {
+                        $arrayParam[$i] = $arrayParam[$i].'|'.$res[0];
+                    }
+                }
+            }
+        }
+    } else {
+        array_push($arrayParam, $param);
+    }
+
+    return $arrayParam;
+}
