89af97efb7ed9c60bdd94a761904fe999eaebd51
dandisy <dandi.webdev@gmail.com>
2019-06-23 20:28:16 +0700

    fixing error
    fixing error seeding while table and database not exist

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $this->call(SettingsTableSeeder::class);
+        // $this->call(SettingsTableSeeder::class);
         $this->call(LaratrustSeeder::class);
     }
 }
